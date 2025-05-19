a67db98b1950581d83dd470fb41de05728c511c8
Jisha <jisha.tomsher@gmail.com>
2025-05-19 17:10:54 +0400

    reminder

--- app/Notifications/VendorDocumentExpiryNotification.php
+++ app/Notifications/VendorDocumentExpiryNotification.php
@@ -6,49 +6,33 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Carbon\Carbon;
 
-class VendorDocumentExpiryNotification extends Notification
+class VendorDocumentExpiryNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
-    /**
-     * Create a new notification instance.
-     */
-    public function __construct()
+    protected $documentName;
+    protected $expiryDate;
+
+    public function __construct($documentName, Carbon $expiryDate)
     {
-        //
+        $this->documentName = $documentName;
+        $this->expiryDate = $expiryDate->format('d M Y');
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @return array<int, string>
-     */
-    public function via(object $notifiable): array
+    public function via($notifiable)
     {
         return ['mail'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail(object $notifiable): MailMessage
+    public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(object $notifiable): array
-    {
-        return [
-            //
-        ];
+            ->subject('Document Expiry Reminder: ' . $this->documentName)
+            ->greeting('Hello ' . $notifiable->name . ',')
+            ->line("Your document **{$this->documentName}** is set to expire on **{$this->expiryDate}**.")
+            ->line('Please update your documents to avoid any disruptions.')
+            ->line('Thank you for staying compliant.');
     }
 }
