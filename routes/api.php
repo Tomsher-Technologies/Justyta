3c5bfaae6bb0d9ab2efa0967352338db6b16c379
Jisha <jisha.tomsher@gmail.com>
2025-06-24 13:38:24 +0400

    services

--- routes/api.php
+++ routes/api.php
@@ -5,56 +5,64 @@ use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\HomeController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ServiceController;
 
 Route::group(['prefix' => 'v1'], function () {
-    Route::middleware('set_api_locale')->group(function () {
-        
-        Route::get('/test', function () {
-            return 'test';
-        });
-
-        Route::post('/login', [AuthController::class, 'login']);
-        Route::post('/user-register', [AuthController::class, 'register']);
-        Route::post('/forget-password', [AuthController::class, 'forgetRequest']);
-        Route::post('/resend-email-otp', [AuthController::class, 'forgetRequest']);
-        Route::post('/verify-email-otp', [AuthController::class, 'verifyOTP']);
-        Route::post('/reset-password', [AuthController::class, 'resetPassword']);
-
-
-        Route::middleware(['ensureFrontendRequestsAreStateful', 'auth:sanctum'])->group(function () {
-            // Route::get('/services', [ServiceController::class, 'index']);
-            Route::post('/logout', [AuthController::class, 'logout']);
-
-            Route::get('/home', [HomeController::class, 'home']);
-            Route::get('/lawfirm-services', [HomeController::class, 'lawfirmServices']);
-            Route::get('/search', [HomeController::class, 'search']);
-
-            //News
-            Route::get('/news', [HomeController::class, 'news']);
-            Route::get('/news-details', [HomeController::class, 'newsDetails']);
-
-            // User Account
-            Route::get('/user-account', [UserController::class, 'account']);
-            Route::post('/edit-profile', [UserController::class, 'editProfile']);
-            Route::post('/change-password', [UserController::class, 'changePassword']);
-            Route::delete('/delete-account', [UserController::class, 'deleteAccount']);
-
-            //Contact US
-            Route::post('/contact-us', [HomeController::class, 'contactUs']);
-
-            // Job Posts
-            Route::get('/job-posts', [JobPostController::class, 'index']);
-        });
-
-        
+Route::middleware('set_api_locale')->group(function () {
+    
+    Route::get('/test', function () {
+        return 'test';
     });
 
-    Route::fallback(function () {
-        return response()->json([
-            'status' => false,
-            'message' => 'Route not found.',
-        ], 404);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/user-register', [AuthController::class, 'register']);
+    Route::post('/forget-password', [AuthController::class, 'forgetRequest']);
+    Route::post('/resend-email-otp', [AuthController::class, 'forgetRequest']);
+    Route::post('/verify-email-otp', [AuthController::class, 'verifyOTP']);
+    Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+
+
+    Route::middleware(['ensureFrontendRequestsAreStateful', 'auth:sanctum'])->group(function () {
+        // Route::get('/services', [ServiceController::class, 'index']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+
+        Route::get('/home', [HomeController::class, 'home']);
+        Route::get('/lawfirm-services', [HomeController::class, 'lawfirmServices']);
+        Route::get('/search', [HomeController::class, 'search']);
+
+        //News
+        Route::get('/news', [HomeController::class, 'news']);
+        Route::get('/news-details', [HomeController::class, 'newsDetails']);
+
+        // User Account
+        Route::get('/user-account', [UserController::class, 'account']);
+        Route::post('/edit-profile', [UserController::class, 'editProfile']);
+        Route::post('/change-password', [UserController::class, 'changePassword']);
+        Route::delete('/delete-account', [UserController::class, 'deleteAccount']);
+
+        //Contact US
+        Route::post('/contact-us', [HomeController::class, 'contactUs']);
+
+        // Job Posts
+        Route::get('/job-posts', [JobPostController::class, 'index']);
+
+        // Get Service Form Contents
+        Route::get('/court-case-submission', [ServiceController::class, 'getCourtCaseFormData']);
+
+
+        // Service Request Submission
+
     });
+
+    
+});
+
+Route::fallback(function () {
+    return response()->json([
+        'status' => false,
+        'message' => 'Route not found.',
+    ], 404);
+});
 });
 
 Route::fallback(function () {
