91f7c9019d45631cf5e0d7e43ff1ff8a9092dbc1
Jisha <jisha.tomsher@gmail.com>
2025-10-10 18:19:55 +0400

    dcd

--- resources/views/layouts/web_lawyer.blade.php
+++ resources/views/layouts/web_lawyer.blade.php
@@ -380,6 +380,8 @@
 
                     // Handle remote participants
                     client.on('user-added', async (payload) => {
+
+                        console.log("User addeddddddddddddddddddddd:", payload);
                         const remoteUser = payload.user || payload;
                         const remoteUserId = remoteUser.userId;
                         console.log("Remote user joined:", remoteUserId);
