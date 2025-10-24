ae08edd04147d2c5aef562d43be5a7eeec9ba555
Jisha <jisha.tomsher@gmail.com>
2025-10-24 14:13:46 +0400

    zoom

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
+    server: {
+        // host: true, // equivalent to 0.0.0.0
+        // port: 5174,
+        // strictPort: true,
+        // https: true,
+        host: 'localhost',
+        port: 5174,
+        strictPort: true,
+    },
+    preview: {
+        host: true,
+        port: 5174,
+        strictPort: true,
+        https: true,
+    },
+    build: {
+        target: 'esnext',
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+    },
     plugins: [
         laravel({
             input: [
@@ -11,20 +40,6 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        // basicSsl()
     ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'resources/js'),
-        },
-    },
-    server: {
-        host: 'localhost',
-        port: 5174,
-        strictPort: true,
-    },
-    build: {
-        commonjsOptions: {
-            transformMixedEsModules: true,
-        },
-    },
 });
