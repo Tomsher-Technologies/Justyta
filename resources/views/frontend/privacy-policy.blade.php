af333a0f876412e2a5a1a28f7a84980bd2abe9d1
Jisha <jisha.tomsher@gmail.com>
2026-01-17 16:14:35 +0400

    responsive

--- resources/views/frontend/privacy-policy.blade.php
+++ resources/views/frontend/privacy-policy.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')    
   
-    <section class="py-[40px] md:py-[0px] px-20 md:px-10 lg:px-20 xl:px-0">
-      <div class="container m-auto px-20">
+    <section class="py-[40px] md:py-[0px] px-0 md:px-10 lg:px-20 xl:px-0">
+      <div class="container m-auto px-5 md:px-20">
          <!-- Title -->
          @php
             $pageData = $page->sections->first();
          @endphp
-         <h1 class="mb-5 text-5xl text-[#034833] font-cinzel text-center">{{ $pageData?->getTranslation('title', $lang) }}</h1>
+         <h1 class="mb-5 text-2xl md:text-5xl text-[#034833] font-cinzel text-center">{{ $pageData?->getTranslation('title', $lang) }}</h1>
          <!-- Meta -->
         
          <!-- Blog Content -->
